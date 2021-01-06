112d23fd5b6c54aee475758a3960b088b14d51bf
SamroodAli <samroodali@live.com>
2021-01-06 12:35:40 +0530

    Add:labels

--- main.py
+++ main.py
@@ -9,12 +9,42 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.minsize(width=500, height=500)
+window.config(padx=20, pady=20)
 
 # ---------------------------- WIDGETS SETUP ------------------------------- #
 # canvas widget
 PASS_IMG = PhotoImage(file="logo.png")
-canvas = Canvas(width=200, height=189)
-canvas.create_image(100, 145, image=PASS_IMG)
-canvas.pack()
+canvas = Canvas(width=200, height=200)
+canvas.config()
+canvas.create_image(100, 100, image=PASS_IMG)
+canvas.grid(column=1, row=0)
+
+# Label
+# Label for Website
+label = Label(text="Website")
+label.grid(column=0, row=1)
+
+# Label for Email/Username
+label = Label(text="Email/Username")
+label.grid(column=0, row=2)
+
+# Label for Password
+label = Label(text="Password")
+label.grid(column=0, row=3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 window.mainloop()
