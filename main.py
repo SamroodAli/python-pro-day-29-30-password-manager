0c71b69a0e8ec549ec23115b4214744346b8e4e3
SamroodAli <samroodali@live.com>
2021-01-06 12:16:06 +0530

    Add:window and canvas for image

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+"""Password Manager Main file"""
+
+# ---------------------------- IMPORTS------------------------------- #
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.minsize(width=500, height=500)
+
+# ---------------------------- WIDGETS SETUP ------------------------------- #
+# canvas widget
+PASS_IMG = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=189)
+canvas.create_image(100, 145, image=PASS_IMG)
+canvas.pack()
+window.mainloop()
